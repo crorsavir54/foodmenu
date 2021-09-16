9a28310e6eac9804fec7c7d6863d95839d1cfe1f
Carlos Rivas <coriv@pm.me>
2021-09-16 15:49:16 +0800

    Initial Commit

---
+++ foodmenu/foodmenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodmenuApp.swift
+//  foodmenu
+//
+//  Created by coriv🧑🏻‍💻 on 9/16/21.
+//
+
+import SwiftUI
+
+@main
+struct foodmenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
